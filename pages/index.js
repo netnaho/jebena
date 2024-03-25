eef74d2ecea45233c6698d2a9c3a7b445aafaecf
netnaho <87896136+netnaho@users.noreply.github.com>
2024-03-25 11:13:13 +0300

    minor fixes

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,51 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import "../styles/Home.module.css";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import Jebena from "./jebena-img.jpg";
 
 export default function Home() {
-  const cart  = [{
-    id : 1,
-    title : "Mens Black SHoes",
-    price : 1200
-    
-  },{
-    id : 2,
-    title : "Womens red dress",
-    price : 3000
-    
-  },{
-    id : 3,
-    title : "Mens Watches",
-    price : 5000
-    
-  }]
+  const cart = [
+    {
+      id: 1,
+      title: "ጀበና ☕",
+      price: 900,
+    },
+  ];
   const handleChapa = async () => {
-		const referenceNumber = uuidv4();
-		const header = {
-            headers: { 
-                "Content-Type": "application/json"
-             },
-            
-        };
-		
-		const body = {
-			amount: cart.reduce((total, item) => total + item.price, 0), //Amount should be integer
-			currency: "ETB",
-			email: "test@gmail.com",
-			first_name: "Eyuel",
-			last_name: "Haile",
-			phone_number: "0917000000", //the phone number must not include +251
-			tx_ref: referenceNumber,
-			callback_url: "http://localhost:3000/success",
-			return_url: "http://localhost:3000/varifyChapa?tnx_ref="+referenceNumber,
-			customization: {
-				title: "Item Payment",
-				description: "Paying for item in the cart"
-			}
-		}
-		let response = await axios.post(`http://localhost:3000/api/chapa`, body, header);
-		//console.log(response.data.resp.data.checkout_url);
-		window.location.href = response.data.data.checkout_url;
-	}
+    const referenceNumber = uuidv4();
+    const header = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = {
+      amount: cart.reduce((total, item) => total + item.price, 0), //Amount should be integer
+      currency: "ETB",
+      email: "test@gmail.com",
+      first_name: "Eyuel",
+      last_name: "Haile",
+      phone_number: "0917000000", //the phone number must not include +251
+      tx_ref: referenceNumber,
+      callback_url: "http://localhost:3000/success",
+      return_url:
+        "http://localhost:3000/varifyChapa?tnx_ref=" + referenceNumber,
+      customization: {
+        title: "Item Payment",
+        description: "Paying for item in the cart",
+      },
+    };
+    let response = await axios.post(
+      `http://localhost:3000/api/chapa`,
+      body,
+      header
+    );
+    //console.log(response.data.resp.data.checkout_url);
+    window.location.href = response.data.data.checkout_url;
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -56,35 +54,39 @@ export default function Home() {
       </Head>
 
       <main>
-
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h3>Checkout Page </h3>
-            {cart.map((item) =>  (
-              <div className={styles.title} style={{justifyContent:"space-between"}}>
-                <span style={{float:"left"}}>{item.title}</span>
-                <span style={{float:"right"}}>{item.price} ETB</span>
+            <Image
+              style={{ width: "200px" }}
+              src={Jebena}
+              alt="jebena image"
+              className=" w-11 ml-7"
+            />
+            {cart.map((item) => (
+              <div
+                className={styles.title}
+                style={{ justifyContent: "space-between" }}
+              >
+                <span
+                  className=" text-lg font-semibold"
+                  style={{ float: "left" }}
+                >
+                  {item.title}
+                </span>
+                <span style={{ float: "right" }}>{item.price} ETB</span>
               </div>
             ))}
-            <div className={styles.title} style={{justifyContent:"space-between"}}>
-                <span style={{float:"left"}}>Total</span>
-                <span style={{float:"right"}}>{cart.reduce((total, item) => total + item.price, 0)} ETB</span>
-            </div>
-            <button
-              type="button"
-              className={styles.btn}
-              onClick={handleChapa}
-            >
-              Pay with 
+            <button type="button" className={styles.btn} onClick={handleChapa}>
+              Buy with
               <img
-                  src="/images/chapa.svg"
-                  alt="chapa image"
-                  style={{
-                    width: "100px",
-                    paddingLeft: "15px",
-                    paddingTop: "5px"
-                  }}
-                />
+                src="/images/chapa.svg"
+                alt="chapa image"
+                style={{
+                  width: "100px",
+                  paddingLeft: "15px",
+                  paddingTop: "5px",
+                }}
+              />
             </button>
           </div>
         </div>
